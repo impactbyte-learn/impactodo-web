6259f33f7ad02021d0471ffce2469dc30506c133
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-02 14:17:43 +0700

    Refine DOM page again

--- scripts/dom.js
+++ scripts/dom.js
@@ -4,10 +4,28 @@
 // console.log(inputFullName);
 // console.log(submitButton);
 
-const pancasilaCollection = document.querySelector("#pancasila").children;
-const pancasila = Array.from(pancasilaCollection);
+const showPancasila = () => {
+  const pancasilaCollection = document.querySelector("#pancasila").children;
+  const pancasila = Array.from(pancasilaCollection);
 
-pancasila.map(silaNode => {
-  const sila = silaNode.innerHTML;
-  console.log(sila.substr(3));
-});
+  pancasila.map(silaNode => {
+    const sila = silaNode.innerHTML;
+    console.log(sila.substr(3));
+  });
+};
+
+const calculateAdd = () => {
+  let number1 = document.getElementById("number1").value;
+  let number2 = document.getElementById("number2").value;
+
+  let result = Number(number1) + Number(number2);
+  console.log(result);
+};
+
+const combineName = () => {
+  let firstname = document.getElementById("firstname").value;
+  let lastname = document.getElementById("lastname").value;
+
+  let result = `${firstname} ${lastname}`;
+  console.log(result);
+};
