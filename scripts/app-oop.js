723144388f17adc686c7708dcc1cdf0a2b8d00ae
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-05 15:18:00 +0700

    Create Todo OOP version

--- scripts/app-oop.js
+++ scripts/app-oop.js
@@ -1,21 +1,40 @@
-function Employee(name, profession) {
-  this.name = name;
-  this.profession = profession;
-}
-
-const fuad = new Employee("Fuad Baskara", "Developer");
-
-console.log(fuad);
-
-// -----------------------------------------------------------------------------
-
-class Boss {
-  constructor(name, profession) {
+class Human {
+  constructor(name, age) {
     this.name = name;
-    this.profession = profession;
+    this.age = age || 1;
+  }
+  walk() {
+    console.log(`${this.name} is walking`);
+  }
+  run() {
+    console.log(`${this.name} is running`);
   }
 }
 
-const dody = new Boss("Stefanus Dody", "Boss Besar");
+class Employee extends Human {
+  constructor(name, age, profession, salary) {
+    super(name, age);
+    this.profession = profession;
+    this.salary = salary;
+  }
+}
 
+class Boss extends Human {
+  constructor(name, age, power, income) {
+    super(name, age);
+    this.power = power;
+    this.income = income;
+  }
+}
+
+const haidar = new Human("Haidar", 24);
+const yanuar = new Employee("Yanuar", 71, "Developer", 10000);
+const dody = new Boss("Dody", 31, "Goreng Gorengan", 123456789);
+
+console.log(haidar);
+console.log(yanuar);
 console.log(dody);
+
+haidar.run();
+yanuar.walk();
+dody.run();
