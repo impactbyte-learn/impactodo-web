1cc41171fa2210b1555c6714257a9a7d5ac6a3a2
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-12 13:02:00 +0700

    Setup for live dev and Heroku

---
+++ index.js
@@ -0,0 +1,12 @@
+const static = require("node-static");
+const file = new static.Server();
+
+require("http")
+  .createServer(function(request, response) {
+    request
+      .addListener("end", function() {
+        file.serve(request, response);
+      })
+      .resume();
+  })
+  .listen(process.env.PORT || 8000);
